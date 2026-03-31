991c521786f203dedc2e44f3ad9f918c345aec18
_ColloTech <kololamoses@gmail.com>
2026-03-31 19:46:04 +0300

    Done ✅ immplemrenting error display and buttton spinner design

--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -1,14 +1,19 @@
 import React,{useState} from 'react'
 import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 function Register() {
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword]  = useState('')
+  const [errors, setError] = useState({})
+  const [success, setSuccess]  = useState(false)
+  const [loading, setLoading]   = useState(false)
 
   const handleRegistration = async (e) => {
     e.preventDefault();
-    
+    setLoading(true)
     //User data object to be sent to the django rest api (serializer) using Axios
     const userData = {
       username, email,password
@@ -17,9 +22,15 @@ function Register() {
        const response = await axios.post('http://127.0.0.1:8000/api/v1/register/', userData)
        console.log(response.data)
        console.log('RegistrationSuccessful!');
+       setError({})
+       setSuccess(true)
     }
     catch(error){
+      setError(error.response.data)
       console.error('Registration error', error.response.data)
+    }
+    finally{
+      setLoading(false)
     }
     
   }
@@ -32,11 +43,26 @@ function Register() {
               <h3 className='text-light text-center pb-3'>Create an Account</h3>
 
               <form onSubmit={handleRegistration}>
+                <div className="mb-3">
+                <input type="text" placeholder='Username' className='form-control' value={username} onChange={(e) => setUsername(e.target.value)}/>
+                <small>{errors.username && <div className="text-danger">{errors.username}</div>}</small>
+                </div>
+                <div className="mb-3">
+                 <input type="email" placeholder='Email'  className='form-control' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                 <small>{errors.email && <div className="text-danger">{errors.email}</div>}</small>
+                </div>
+                <div className="mb-3">
+                 <input type="password"  placeholder='Set Password' className='form-control' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                 <small>{errors.password && <div className="text-danger">{errors.password}</div>}</small>
+                </div>
+                {success && <div className='alert alert-success'>Registration Successful!</div>}
+                {loading ? (
+                  <button type='submit' className='btn btn-info d-block mx-auto' disabled> <FontAwesomeIcon icon={faSpinner} spin/> Please Wait...</button>
+                ) : (
 
-                <input type="text" placeholder='Username' className='form-control mb-3' value={username} onChange={(e) => setUsername(e.target.value)}/>
-                <input type="email" placeholder='Email'  className='form-control mb-3' value={email} onChange={(e) => setEmail(e.target.value)}/>
-                <input type="password"  placeholder='Set Password' className='form-control mb-5' value={password} onChange={(e) => setPassword(e.target.value)}/>
-                <button type='submit' className='btn btn-info d-block mx-auto' >Register</button>
+                  <button type='submit' className='btn btn-info d-block mx-auto' >Register</button>
+                )}
+              
               </form>
             </div>
         </div>
